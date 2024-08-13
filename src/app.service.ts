3ae646afe6cc01d277174b4afdcd5a76d4763119
v-yupeng <v-yupeng@shuwen.com>
2024-08-13 10:25:05 +0800

    q

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+  getMysqlPwd(): string {
+    // 获取环境变量的值
+    const pwd = this.configService.get<string>('mysql_pwd');
+    return pwd;
   }
 }
